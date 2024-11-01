963e1ab72ca04662a1c30f1afc6f90ba86c892c0
NedzunganiK <Nedzunganik8@gmail.com>
2024-11-01 14:56:55 +0200

    Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""Hypermedia pagination sample.
+"""Simple pagination sample.
 """
 import csv
-import math
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -45,19 +44,3 @@ class Server:
         if start > len(data):
             return []
         return data[start:end]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        """Retrieves information about a page.
-        """
-        page_data = self.get_page(page, page_size)
-        start, end = index_range(page, page_size)
-        total_pages = math.ceil(len(self.__dataset) / page_size)
-        page_info = {
-            'page_size': len(page_data),
-            'page': page,
-            'data': page_data,
-            'next_page': page + 1 if end < len(self.__dataset) else None,
-            'prev_page': page - 1 if start > 0 else None,
-            'total_pages': total_pages,
-        }
-        return page_info
