973340dfbe4813b044c538e6a1c256eb45ad1936
NedzunganiK <Nedzunganik8@gmail.com>
2024-11-08 12:24:25 +0200

    i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-""" doc doc doc """
-from flask import Flask, render_template, request
+"""
+Module for a basic flask application
+"""
+from flask import Flask
+from flask import request
 from flask_babel import Babel
+from flask import render_template
 
 
-class Config(object):
-    """doc doc doc"""
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+class Config:
+    """
+    Represents application config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
@@ -17,19 +22,24 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Represents function for  a basic html template
+    """
+    return render_template('4-index.html')
+
+
 @babel.localeselector
-def get_locale() -> str:
-    """ doc doc doc """
-    if request.args.get("locale") in app.config["LANGUAGES"]:
-        return request.args.get("locale")
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+def get_locale():
+    """
+    Represents function to get locale from request object
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route("/")
-def index() -> str:
-    """doc doc doc"""
-    return render_template("4-index.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run()
--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-""" doc doc doc """
-from flask import Flask, render_template, request, g
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
-class Config(object):
-    """doc doc doc"""
-
+class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -14,43 +15,31 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
-def get_user() -> dict:
-    """doc doc doc"""
-    user_id = request.args.get("login_as")
-    if user_id is not None and int(user_id) in users:
-        return users[int(user_id)]
-    return None
-
-
-@app.before_request
-def before_request():
-    """doc doc doc"""
-    g.user = get_user()
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """doc doc doc"""
-    if request.args.get("locale") in app.config["LANGUAGES"]:
-        return request.args.get("locale")
+    """Retrieves the locale for a web page.
+    """
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/")
-def index() -> str:
-    """doc doc doc"""
-    return render_template("5-index.html")
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('4-index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
